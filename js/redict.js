6692968207ddde9b1c17ebb3717690a846e04ba5
ck <75999325+ckoglu@users.noreply.github.com>
2025-11-10 16:38:01 +0300

    Add files via upload

---
+++ js/redict.js
@@ -0,0 +1,24 @@
+// --- Yönlendirme ve Bilgi Fonksiyonları ---
+function goToSeries() {
+    window.location.href = 'dizi.html';
+}
+
+function goToDocumentaries() {
+    window.location.href = 'belgesel.html';
+}
+
+function goToLiveTV() {
+    window.location.href = 'canli.html';
+}
+
+function goToSearch() {
+    window.location.href = 'ara.html';
+}
+
+function goToFilms() {
+    window.location.href = 'film.html';
+}
+
+function showInfo() {
+    alert('STREAMTV - Sınırsız eğlence platformu. Binlerce film, dizi ve canlı TV kanalına erişim sağlayın.');
+}
